1f7076064a58809c71c89b863106d8b328a5fae4
Tony Powell <adpowell@mtu.edu>
2017-04-25 09:02:18 -0400

    Fixed question sorting logic

--- js/controllers/examController.js
+++ js/controllers/examController.js
@@ -7,31 +7,39 @@ mainApp.controller("examCtrl", function($scope, $http, $cookies, examService){
     }
 
     $scope.exam = exam;
+    $scope.loadingQuestions = true;
+    retrieveQuestions(exam.name);
 
     function retrieveQuestions(name) {
         $http.post('php/examQuestions', {name:name})
             .then(function(response){
+                console.log('retrieveQuestions:',response);
                 if (response.data.success) {
                     $scope.questions = [];
-                    var sortedQuestions = response.data.questions.slice();
+
+                    // Sort the incoming questions so their numbers line up
+                    var sortedQuestions = response.data.results.questions.slice();
                     sortedQuestions.sort(function(a, b){
                       return a.number - b.number;
                     });
+
+                    // For each question, add an array of its choices as a property
                     for (var i = 0; i < sortedQuestions.length; i++) {
                       $scope.questions.push(sortedQuestions[i]);
                       $scope.questions[$scope.questions.length - 1].choices = [];
-                      for (var j = 0; j < response.data.choices.length; j++) {
-                        if (response.data.choices[i].Questions_number == i + 1) {
-                          $scope.questions[$scope.questions.length - 1]
-                            .push(response.data.choices[j]);
+                      for (var j = 0; j < response.data.results.choices.length; j++) {
+                        if (response.data.results.choices[j].Questions_number == i + 1) {
+                          $scope.questions[$scope.questions.length - 1].choices
+                            .push(response.data.results.choices[j]);
                         }
                       }
                     }
+                    console.log('retrieveQuestions:', $scope.questions);
+                    $scope.loadingQuestions = false;
                 } else {
                     alert(response.data.message);
                     window.location.href = 'dashboard';
                 }
             });
-            console.log('retrieveQuestions:', $scope.questions);
     }
 });
