1b2f7d765373e9bf1f356cfffebbd17835ffb4e7
Tony Powell <adpowell@mtu.edu>
2017-04-25 08:50:54 -0400

    Retrieve exam questions from DB

--- js/controllers/examController.js
+++ js/controllers/examController.js
@@ -8,15 +8,30 @@ mainApp.controller("examCtrl", function($scope, $http, $cookies, examService){
 
     $scope.exam = exam;
 
-    function retrieveQuestions(name, created) {
-        $http.post('php/examQuestions', {name:name, created:created})
+    function retrieveQuestions(name) {
+        $http.post('php/examQuestions', {name:name})
             .then(function(response){
                 if (response.data.success) {
-                    $scope.questions = response.data.questions;
+                    $scope.questions = [];
+                    var sortedQuestions = response.data.questions.slice();
+                    sortedQuestions.sort(function(a, b){
+                      return a.number - b.number;
+                    });
+                    for (var i = 0; i < sortedQuestions.length; i++) {
+                      $scope.questions.push(sortedQuestions[i]);
+                      $scope.questions[$scope.questions.length - 1].choices = [];
+                      for (var j = 0; j < response.data.choices.length; j++) {
+                        if (response.data.choices[i].Questions_number == i + 1) {
+                          $scope.questions[$scope.questions.length - 1]
+                            .push(response.data.choices[j]);
+                        }
+                      }
+                    }
                 } else {
                     alert(response.data.message);
                     window.location.href = 'dashboard';
                 }
             });
+            console.log('retrieveQuestions:', $scope.questions);
     }
 });
